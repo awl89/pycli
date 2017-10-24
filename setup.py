3c0fe35de9a9dcd3989499a7cd3f60b05bbb7b01
Andreas Lykke <awl@uber.com>
2017-10-24 13:20:24 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pycli',
+  packages = ['pycli'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Andreas Winther Lykke',
+  author_email = 'andreas.winther.lykke@gmail.com',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
